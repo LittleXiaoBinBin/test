fd2bc0cf8fd66145bd246477fa206ac37a329df5
HeLianYun <1255897236@qq.com>
2018-09-23 13:22:14 +0800

    s

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -5,7 +5,6 @@ public class test {
     public static void main(String[] args) {
         Person person = new Person("nin", 18);
         Person person1 = new Person("nin", 18);
-
         System.out.println(person);
 
     }
